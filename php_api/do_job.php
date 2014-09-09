731f54047472c5fc82818bf7b759efa5fba85d59
OPolpo <git-projects@omnip.it>
2014-09-09 23:40:26 +0200

    do_job check if frame is on a turn

--- php_api/do_job.php
+++ php_api/do_job.php
@@ -1,16 +1,34 @@
 <?php
     require_once 'utils.php';
-    $input = '';
-    $i=0;
     $dir = $_SERVER["argv"][1];
+
+    function detect_turn($point){
+        if($point['t'] === 'r')
+            return 'r';
+        if ($point['t'] === 'l')
+            return 'l';
+        return 0;
+    }
+
+    $i=0;
     $job = json_decode(file_get_contents($dir.$job_file_name), true);
     $points = $job['points'];
+    $input = '';
     foreach ($points as $point){
         $input = $url.'&location='.$point['l'].','.$point['b'].'&heading='.($point['h']).'&pitch=0&key='.$apikey;
         $content = file_get_contents($input);
-        file_put_contents($dir.str_pad($i, 8, '0', STR_PAD_LEFT).'-'.$point['l'].'-'.$point['b'].'.jpg', $content);
-        $input='';
-        $i+=1;
+        $filename = $dir.str_pad($i, 8, '0', STR_PAD_LEFT).'-'.$point['l'].'-'.$point['b'];
+        $turn = detect_turn($point);
+        if($turn === 'r'){
+            $filename .= "[RIGHT]";
+        }
+        else if($turn === 'l'){
+            $filename .= "[LEFT]";
+        }
+        $filename .= '.jpg';
+        file_put_contents($filename, $content);
+        $input = '';
+        $i += 1;
     }
     if(!is_dir($output_dir)){
         mkdir($output_dir);
